b535b8734ba26901c5ceb9fa1e19ffed07f412b2
jenna-lab <jennifer.sammy@student.moringaschool.com>
2023-02-13 22:48:03 +0300

    completed assignment

--- lib/song.rb
+++ lib/song.rb
@@ -26,25 +26,10 @@ class Song
     end
 
     def self.genre_count
-        genre_count = {}
-        @@genres.each do |genre|
-            if genre_count[genre]
-                genre_count[genre] += 1
-            else
-                genre_count[genre] = 1
-            end
-        end
-        genre_count
+        @@genres.tally
     end
+    
     def self.artist_count
-        artist_count = {}
-        @@artists.each do |artist|
-            if artist_count[artist]
-                artist_count[artist] += 1
-            else
-                artist_count[artist] = 1
-            end
-        end
-        artist_count
+        @@artists.tally
     end
 end
